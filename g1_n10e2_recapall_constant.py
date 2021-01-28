705a35fa9389357c4538fe35ee95551cedf0757e
nerdbrained <nerdbrained@gmail.com>
2021-01-28 17:36:39 -0500

    initial commit

---
+++ g1_n10e2_recapall_constant.py
@@ -0,0 +1,239 @@
+###recapitate SLiMulations with constant population size
+
+import pyslim
+import msprime
+import tskit
+import multiprocessing
+import os
+
+### number of breeders
+nbreeder = 100
+
+###ancient scenario
+scenario="constant"
+
+###specify input files
+dir="/scratch/br450/SLiM_ped_g1/"
+
+###scenario
+simname="g1_n10e2_td100_l99/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
+
+
+
+###scenario
+simname="g1_n10e2_td100_l95/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
+
+
+
+###scenario
+simname="g1_n10e2_td130_l99/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
+
+
+
+###scenario
+simname="g1_n10e2_td130_l95/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
+
+
+
+###scenario
+simname="g1_n10e2_td160_l99/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
+
+
+
+###scenario
+simname="g1_n10e2_td160_l95/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
+
+
+
+###scenario
+simname="g1_n10e2_td190_l99/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
+
+
+
+###scenario
+simname="g1_n10e2_td190_l95/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
+
+
+
+###scenario
+simname="g1_n10e2_constant/"
+
+###output file directory
+outdir=dir+simname+scenario+"_recap/"
+
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+def run_recap(i,c):
+	tsname=dir+simname+"i"+str(i)+"_chr_"+str(c)+".trees"
+	outfile=outdir+"i"+str(i)+"_chr_"+str(c)+"_recap.trees"
+	slim_ts = pyslim.load(tsname)
+	recap_constant_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=nbreeder)
+	recap_constant_ts.dump(outfile)
+
+#recapitate all slimulations in parallel
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_recap, zip(its,chrs))
---
+++ g1_n10e2_treeseqall.py
@@ -0,0 +1,273 @@
+###Python/pyslim code for generating tree sequences under recent demographic decline scenarios
+
+##Import needed packages
+import subprocess
+import multiprocessing
+import os
+
+##set up needed paths
+dir="/scratch/br450/SLiM_ped_g1/"
+
+slim_template= dir + "g1_treeseq_template.slim"
+
+##set up directory for scenario: nhistoric=10e2, tdecline=100, lambda=99
+output_dir= dir + "g1_n10e2_td100_l99/"
+
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+##set up function for running a given demographic scenario: nhistoric=10e2, tdecline=100, lambda=99
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
+
+##set up directory for scenario: nhistoric=10e2, tdecline=100, lambda=99
+output_dir= dir + "g1_n10e2_td100_l95/"
+
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+##set up function for running a given demographic scenario: nhistoric=10e2, tdecline=100, lambda=95
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
+
+
+
+
+##set up directory for scenario: nhistoric=10e2, tdecline=130, lambda=99
+output_dir= dir + "g1_n10e2_td130_l99/"
+
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+##set up function for running a given demographic scenario: nhistoric=10e2, tdecline=130, lambda=99
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
+
+##set up directory for scenario: nhistoric=10e2, tdecline=130, lambda=99
+output_dir= dir + "g1_n10e2_td130_l95/"
+
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+##set up function for running a given demographic scenario: nhistoric=10e2, tdecline=130, lambda=95
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
+
+
+
+
+
+##set up directory for scenario: nhistoric=10e2, tdecline=160, lambda=99
+output_dir= dir + "g1_n10e2_td160_l99/"
+
+##set up function for running a given demographic scenario: nhistoric=10e2, tdecline=160, lambda=99
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
+
+##set up directory for scenario: nhistoric=10e2, tdecline=160, lambda=99
+output_dir= dir + "g1_n10e2_td160_l95/"
+
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+##set up function for running a given demographic scenario: nhistoric=10e2, tdecline=160, lambda=95
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
+
+
+
+
+##set up directory for scenario: nhistoric=10e2, tdecline=190, lambda=99
+output_dir= dir + "g1_n10e2_td190_l99/"
+
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+##set up function for running a given demographic scenario: nhistoric=10e2, tdecline=190, lambda=99
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
+
+##set up directory for scenario: nhistoric=10e2, tdecline=190, lambda=99
+output_dir= dir + "g1_n10e2_td190_l95/"
+
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+##set up function for running a given demographic scenario: nhistoric=10e2, tdecline=190, lambda=95
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
+
+
+
+##set up directory for scenario: nhistoric=10e2, constant
+output_dir= dir + "g1_n10e2_constant/"
+
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+##set up function for running a given demographic scenario: nhistoric=10e2, constant
+def run_ts(i,c):
+	t0F=output_dir+"i"+str(i)+"_t0samps.txt"
+	t30F=output_dir+"i"+str(i)+"_t30samps.txt"
+	t60F=output_dir+"i"+str(i)+"_t60samps.txt"
+	t90F=output_dir+"i"+str(i)+"_t90samps.txt"
+	t120F=output_dir+"i"+str(i)+"_t120samps.txt"
+	MOF=output_dir+"i"+str(i)+"_matings.txt"
+	TSOF=output_dir+"i"+str(i)+"_chr_"+str(c)+".trees"
+	GOF=output_dir+"i"+str(i)+"_generationfile.txt"
+	try:
+		subprocess.check_output(["/home/br450/build/slim","-m","-d","Nhistoric=100","-d","t0sampfile='"+t0F+"'","-d","t30sampfile='"+t30F+"'","-d","t60sampfile='"+t60F+"'","-d","t90sampfile='"+t90F+"'","-d","t120sampfile='"+t120F+"'","-d","matingfile='"+MOF+"'","-d","treeseqfile='"+TSOF+"'","-d","generationfile='"+GOF+"'",slim_template])
+	except Exception as e:
+		return e
+
+##run five iterations
+a_pool = multiprocessing.Pool()
+its=list(range(1,6))*25
+its.sort()
+chrs=list(range(1,26))*5
+result = a_pool.starmap(run_ts, zip(its,chrs))
